90d3b0d5cb1c69c3b8ae17af18184a83c026c533
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-11-25 19:02:07 -0500

    Fix improper use of LOAD-TIME-VALUE.

--- main.lisp
+++ main.lisp
@@ -1,12 +1,16 @@
 (in-package #:first-time-value)
 
 (defmacro first-time-value (form)
-  (let ((cache-var (gensym (string '#:cache))))
-    `(let ((,cache-var (load-time-value (cons nil nil))))
-       (if (car ,cache-var)
-           (cdr ,cache-var)
-           (prog1 (setf (cdr ,cache-var) ,form)
-             (setf (car ,cache-var) t))))))
+  (let ((cache-var (gensym (string '#:cache)))
+        (gensym (gensym (string '#:gensym))))
+    `(progn
+       (load-time-value (setf (symbol-value ',gensym) (cons nil nil)))
+       (let ((,cache-var (symbol-value ',gensym)))
+         (declare (type cons ,cache-var))
+         (if (car ,cache-var)
+             (cdr ,cache-var)
+             (prog1 (setf (cdr ,cache-var) ,form)
+               (setf (car ,cache-var) t)))))))
 
 (defmacro first-time-values (form)
   `(values-list (first-time-value (multiple-value-list ,form))))
